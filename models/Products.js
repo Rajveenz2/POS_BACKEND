3b37604a66a79e09ebdee4b2a172b0627433689c
Rajveender Singh <72578341+Rajveenz2@users.noreply.github.com>
2023-02-01 21:28:03 +0800

    add Products.js

---
+++ models/Products.js
@@ -0,0 +1,20 @@
+"use strict";
+var mongoose = require("mongoose");
+var db = require("../userDB");
+// var aggregatePaginate = require('mongoose-aggregate-paginate-v2');
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+var ProductSchema = new mongoose.Schema({
+  productName: String,
+  productPrice: String,
+  productImageUrl: String,
+  productDesc: String,
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  productCategory: String,
+  dummy: Boolean,
+});
+
+ProductSchema.plugin(mongoosePaginate);
+ProductSchema.index({ productName: 1 }, { unique: true });
+module.exports = db.model("Product", ProductSchema);
